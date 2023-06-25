814c8304085af567d05e6bbb0e7864cef0c80146
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2023-06-25 17:29:29 -0300

    DAO update

--- exercicios-jpa/src/infra/DAO.java
+++ exercicios-jpa/src/infra/DAO.java
@@ -7,21 +7,21 @@ import javax.persistence.TypedQuery;
 
 import java.util.List;
 
-public class DAO <E> {
+public class DAO<E> {
 
     private static EntityManagerFactory emf;
     private EntityManager em;
     private Class<E> classe;
 
-    static{
-        try{
+    static {
+        try {
             emf = Persistence.createEntityManagerFactory("exercicios-jpa");
-        }catch(Exception e){
-            //logar -> log4j
+        } catch (Exception e) {
+            // logar -> log4j
         }
     }
-    
-    public DAO(){
+
+    public DAO() {
         this(null);
     }
 
@@ -49,24 +49,31 @@ public class DAO <E> {
         return this.abrirTransacao().incluir(entidade).fecharTransacao();
     }
 
-    public E obterPorID(Object id){
+    public E obterPorID(Object id) {
         return em.find(classe, id);
     }
 
-    public List<E> obterTodos(){//Sobrecarga de método.
+    public List<E> obterTodos() {// Sobrecarga de método.
         return this.obterTodos(10, 0);
     }
 
-
-    public List<E> obterTodos(int qtde, int deslocamento){
-        if(classe == null){
+    public List<E> obterTodos(int qtde, int deslocamento) {
+        if (classe == null) {
             throw new UnsupportedOperationException("Classe nula.");
         }
-        
+
         String jpql = "select e from " + classe.getName() + " e";
         TypedQuery<E> query = em.createQuery(jpql, classe);
         query.setMaxResults(qtde);
         query.setFirstResult(deslocamento);
+        return query.getResultList();
+    }
+
+    public List<E> consultar(String nomeConsulta, Object... params) {
+        TypedQuery<E> query = em.createNamedQuery(nomeConsulta, classe);
+        for (int i = 0; i < params.length; i += 2) {
+            query.setParameter(params[i].toString(), params[i + 1]);
+        }
         return query.getResultList();
     }
 
